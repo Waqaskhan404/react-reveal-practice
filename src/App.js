1e737aa708237c809d33dfc3f744414007774ba5
Waqas-CST <waqas.khan.40004@gmail.com>
2023-01-31 17:02:36 +0500

    First update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import {Rotate} from 'react-reveal';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Rotate>
+        <h1>Welcome</h1>
+      </Rotate>
+      
     </div>
   );
 }
